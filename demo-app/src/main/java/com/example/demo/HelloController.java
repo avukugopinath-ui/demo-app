5b15583f1e29f48c7d1985f4694de608481fbbaf
avukugopinath-ui <avukugopinath@gmail.com>
2026-05-04 12:10:23 +0530

    initila commit with change

--- demo-app/src/main/java/com/example/demo/HelloController.java
+++ demo-app/src/main/java/com/example/demo/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Hello from Spring Boot!";
+        return "Hello from Spring Boot, Update to Git Hub!";
     }
 }
